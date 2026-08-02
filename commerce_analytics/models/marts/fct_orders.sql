278b7cc1da8099661fcb1fa229d15974ac692313
saitoou <kouhei02.3110@gmail.com>
2026-08-02 19:52:21 +0900

    matsの追加

---
+++ commerce_analytics/models/marts/fct_orders.sql
@@ -0,0 +1,16 @@
+select
+    order_id,
+    customer_id,
+    order_status,
+    total_amount,
+    ordered_at,
+    updated_at,
+    cancelled_at,
+    is_cancelled,
+
+    case
+        when is_cancelled then 0::number(12, 2)
+        else total_amount
+    end as recognized_order_amount
+
+from {{ ref('stg_postgres__orders') }}
---
+++ commerce_analytics/models/marts/mart_daily_sales.sql
@@ -0,0 +1,17 @@
+select
+    ordered_at::date as order_date,
+
+    count(*) as order_count,
+
+    count_if(not is_cancelled) as valid_order_count,
+
+    count_if(is_cancelled) as cancelled_order_count,
+
+    sum(total_amount)::number(14, 2) as gross_order_amount,
+
+    sum(recognized_order_amount)::number(14, 2)
+        as recognized_order_amount
+
+from {{ ref('fct_orders') }}
+group by
+    ordered_at::date
