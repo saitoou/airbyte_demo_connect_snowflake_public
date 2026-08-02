a5fd60f005f4ef85ce33e2c03e6871ec041c0b69
saitoou <kouhei02.3110@gmail.com>
2026-08-02 19:52:07 +0900

    stagingの追加

---
+++ commerce_analytics/models/staging/postgres/stg_postgres__customers.sql
@@ -0,0 +1,28 @@
+with source as (
+
+  select *
+  from {{ source('postgres', 'customers') }}
+
+),
+
+renamed as (
+
+  select
+    id::number(38, 0) as customer_id,
+    name::varchar as customer_name,
+    email::varchar as email,
+
+    created_at::timestamp_tz as created_at,
+    updated_at::timestamp_tz as updated_at,
+    deleted_at::timestamp_tz as deleted_at,
+
+    deleted_at is not null as is_deleted,
+
+    _airbyte_extracted_at::timestamp_tz as airbyte_extracted_at
+
+  from source
+
+)
+
+select *
+from renamed
---
+++ commerce_analytics/models/staging/postgres/stg_postgres__orders.sql
@@ -0,0 +1,30 @@
+with source as (
+
+  select *
+  from {{ source('postgres', 'orders') }}
+
+),
+
+renamed as (
+
+  select
+    id::number(38, 0) as order_id,
+    customer_id::number(38, 0) as customer_id,
+
+    status::varchar as order_status,
+    total_amount::number(12, 2) as total_amount,
+
+    ordered_at::timestamp_tz as ordered_at,
+    updated_at::timestamp_tz as updated_at,
+    cancelled_at::timestamp_tz as cancelled_at,
+
+    status = 'cancelled' as is_cancelled,
+
+    _airbyte_extracted_at::timestamp_tz as airbyte_extracted_at
+
+  from source
+
+)
+
+select *
+from renamed
